34d0386dba2b23049ba8e3aa5859d09315af7129
forestbaba <hawlahethan@gmail.com>
2022-07-12 08:05:38 +0100

    update

--- src/components/Orders/Tabs.js
+++ src/components/Orders/Tabs.js
@@ -274,6 +274,7 @@ const Tabs = () => {
   }
 
   const handleSearchButton = async (s) => {
+    console.log('*************',s)
 
     const filteredDatax = await s.trim().length > 0 && pendingOrder &&
       pendingOrder.filter(entry => {
